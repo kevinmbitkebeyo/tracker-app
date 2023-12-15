b50a9c33ecca44da84e0b16dc05ec6cc11fefe59
Kevin Mbitkebeyo <mbitkebeyokevin@gmail.com>
2023-12-15 10:55:01 -0500

    style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,22 +1,42 @@
-import Link from 'next/link'
-import React from 'react'
-import {AiFillBug, AiFillBank} from 'react-icons/ai'
+"use client";
+
+import classNames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug, AiFillBank } from "react-icons/ai";
 
 const NavBar = () => {
+  const currentPath = usePathname();
 
-    const links = [
-        {label: 'Dashboard', href:'/'},
-        {label:'Issues', href:'/issues'}
-    ]
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-        <Link href="/"> <AiFillBank /> </Link>
-        <ul className='flex space-x-6'>
-            {links.map(link => <Link key={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>  {link.label}</Link>)}
-            
-        </ul>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        {" "}
+        <AiFillBank />{" "}
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {" "}
+            {link.label}
+          </Link>
+        ))}
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
---
+++ app/issues/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IssuesPages = () => {
+  return (
+    <div>Issues Pages</div>
+  )
+}
+
+export default IssuesPages
