8f5068b79c80f9e8db97bbb9188cf49ee0488507
Kevin Mbitkebeyo <mbitkebeyokevin@gmail.com>
2023-12-16 09:00:39 -0500

    Extract the error message component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ErrorMessage from "@/app/components/ErrorMessage";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Callout, Text, TextField } from "@radix-ui/themes";
@@ -11,47 +12,53 @@ import { Controller, useForm } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import { z } from "zod";
 
- type IssueForm = z.infer<typeof createIssueSchema>
- 
+type IssueForm = z.infer<typeof createIssueSchema>;
+
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit, formState:{errors} } = useForm<IssueForm>({
-    resolver:zodResolver(createIssueSchema)
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
   });
-  const [error, setError] = useState('')
+  const [error, setError] = useState("");
   return (
     <div className="max-w-xl">
-       {error && <Callout.Root color="red" className="mb-5">
-        <Callout.Text>{error} </Callout.Text>
-        </Callout.Root>}
-    <form
-      className="space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-
-        try {
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error} </Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
             await axios.post("/api/issues", data);
             router.push("/issues");
-            
-        } catch (error) {
-            setError('An unexpected error occurred')
-        }
-      })}
-    >
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register("title")} />
-      </TextField.Root>
-      {errors.title && <Text color="red" as="p"> {errors.title.message} </Text>}
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
-      {errors.description && <Text color="red" as="p"> {errors.description.message} </Text>}
+          } catch (error) {
+            setError("An unexpected error occurred");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        <ErrorMessage> {errors.title?.message} </ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
 
-      <Button>Submit New Issue</Button>
-    </form>
+        <ErrorMessage> {errors.description?.message} </ErrorMessage>
+
+        <Button>Submit New Issue</Button>
+      </form>
     </div>
   );
 };
