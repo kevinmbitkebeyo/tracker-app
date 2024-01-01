95bef6ee6c39ed3b8dc6ef047fe1667b49c11f37
Kevin Mbitkebeyo <mbitkebeyokevin@gmail.com>
2024-01-01 09:59:34 -0500

    Optimize performance using react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,18 +7,19 @@ import DeleteIssueButton from "./DeleteIssueButton";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/auth/authOptions";
 import AssigneeSelect from "./AssigneeSelect";
+import { cache } from "react";
 
 interface Props {
   params: { id: string };
 }
 
+const fetchUser = cache( (issueId: number) => 
+  prisma.issue.findUnique({ where: { id: issueId } })
+);
 const IssueDetailPage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions);
 
- const session = await getServerSession(authOptions)
-
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchUser(parseInt(params.id));
 
   if (!issue) notFound();
 
@@ -27,23 +28,25 @@ const IssueDetailPage = async ({ params }: Props) => {
       <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      {session && <Box>
-        <Flex direction="column" gap="4">
-          <AssigneeSelect issue={issue}/>
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>}
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <AssigneeSelect issue={issue} />
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   );
 };
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
-  if (!issue) notFound();
-  return { title: `Issue - ${issue?.title}`, description: 'Details of issue '+issue?.id };
+  const issue = await fetchUser(parseInt(params.id));
+  return {
+    title: `Issue - ${issue?.title}`,
+    description: "Details of issue " + issue?.id,
+  };
 }
 
 export default IssueDetailPage;
